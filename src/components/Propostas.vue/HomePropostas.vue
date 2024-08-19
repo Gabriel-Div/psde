1256a6524a56b4183ea1914807e167bfced9a3fe
Gabriel Rocha Sarno Teles <gabrielrochasarnoteles@gmail.com>
2024-08-19 11:43:18 -0300

    version: 1.0.2 (spell check)

--- src/components/Propostas.vue/HomePropostas.vue
+++ src/components/Propostas.vue/HomePropostas.vue
@@ -12,18 +12,14 @@
                         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">DESEMPREGO
                         </h1>
                         <p class="mt-6 text-xl leading-8 text-gray-700">
-                            Nosso governo combaterá o desemprego oferecendo incentivos fiscais para
-                            empresas que contratem em setores estratégicos e investindo em educação e
-                            treinamento profissional para qualificar trabalhadores. Facilitaremos crédito,
-                            reduziremos tributos e simplificaremos a burocracia para pequenas e médias
-                            empresas, promovendo a criação de empregos.
-                            Ajustaremos as leis trabalhistas
-                            para maior flexibilidade nas contratações e desenvolveremos políticas de inclusão
-                            social para grupos marginalizados. Além disso, reduziremos
-                            impostos sobre bens de consumo para aumentar a
-                            demanda e apoiamos o consumo de produtos
-                            locais, estimulando a produção interna e a
-                            geração de empregos.
+                            Nosso governo combaterá o desemprego oferecendo incentivos fiscais para empresas que
+                            contratem em setores estratégicos, além de investir em educação e treinamento profissional
+                            para qualificar trabalhadores. Facilitaremos o acesso ao crédito, reduziremos tributos e
+                            simplificaremos a burocracia para pequenas e médias empresas, promovendo a criação de
+                            empregos. Ajustaremos as leis trabalhistas para garantir maior flexibilidade nas
+                            contratações e desenvolveremos políticas de inclusão social para grupos marginalizados. Além
+                            disso, reduziremos os impostos sobre bens de consumo para aumentar a demanda e apoiaremos o
+                            consumo de produtos locais, estimulando a produção interna e a geração de empregos.
                         </p>
                     </div>
                 </div>
@@ -43,16 +39,14 @@
                         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">PREÇOS
                         </h1>
                         <p class="mt-6 text-xl leading-8 text-gray-700">
-                            Para reduzir o preço dos alimentos, nosso governo implementará a criação
-                            de Centros Agroindustriais Regionais para processar alimentos próximos
-                            às áreas de produção, reduzindo custos de transporte. Incentivaremos
-                            parcerias público-privadas, oferecendo incentivos fiscais e subsídios para
-                            que empresas e cooperativas locais gerenciem esses centros.
-                            Facilitaremos a integração direta entre produtores e consumidores,
-                            reduzindo intermediários através de plataformas digitais e feiras locais.
-                            Espera-se que essas medidas diminuam os custos de processamento e
-                            transporte, promovam o desenvolvimento econômico regional e contribuam
-                            para a sustentabilidade ao reduzir a pegada de carbono.
+                            Para reduzir o preço dos alimentos, nosso governo implementará a criação de Centros
+                            Agroindustriais Regionais para processar alimentos próximos às áreas de produção, reduzindo
+                            os custos de transporte. Incentivaremos parcerias público-privadas, oferecendo incentivos
+                            fiscais e subsídios para que empresas e cooperativas locais possam gerenciar esses centros.
+                            Facilitaremos a integração direta entre produtores e consumidores, reduzindo o número de
+                            intermediários por meio de plataformas digitais e feiras locais. Espera-se que essas medidas
+                            diminuam os custos de processamento e transporte, promovam o desenvolvimento econômico
+                            regional e contribuam para a sustentabilidade, ao reduzir a pegada de carbono.
                         </p>
                     </div>
                 </div>
@@ -73,13 +67,13 @@
                         </h1>
                         <p class="mt-6 text-xl leading-8 text-gray-700">
                             Para melhorar a sustentabilidade ambiental, nosso governo implementará as seguintes medidas:
-                            Construiremos um linhão em Roraima para garantir a autossuficiência energética do Brasil e
+                            construiremos um linhão em Roraima para garantir a autossuficiência energética do Brasil e
                             reduzir a dependência da Venezuela na região, eliminando a necessidade de termoelétricas.
                             Legalizaremos o garimpo com regulamentação rigorosa, exigindo CNPJ e licenciamento, e
-                            aplicaremos impostos semelhantes aos das empresas estrangeiras, proibindo novas empresas de
-                            fora. Reduziremos em 80% os impostos sobre as placas solares, exigindo que 10% da geração
-                            de energia seja repassada à companhia elétrica. Além disso, acabaremos com o crédito de
-                            carbono, priorizando soluções mais eficazes para a preservação ambiental.
+                            aplicaremos impostos semelhantes aos das empresas estrangeiras, proibindo a entrada de novas
+                            empresas de fora. Reduziremos em 80% os impostos sobre as placas solares, exigindo que 10%
+                            da geração de energia seja repassada à companhia elétrica. Além disso, eliminaremos o
+                            crédito de carbono, priorizando soluções mais eficazes para a preservação ambiental.
                         </p>
                     </div>
                 </div>
@@ -99,15 +93,15 @@
                         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">ECONOMIA
                         </h1>
                         <p class="mt-6 text-xl leading-8 text-gray-700">
-                            Para fortalecer a economia, nosso governo adotará as seguintes medidas: Zeraremos o imposto
-                            sobre a cesta básica, tornando alimentos essenciais mais acessíveis. Reduziremos o imposto
-                            geral para estimular a demanda, o que gerará mais empregos. Eliminaremos o IPI e outros
-                            impostos similares para aliviar o custo de produtos. Além disso, reformularemos o Bolsa
-                            Família, garantindo que os beneficiários recebam o salário integralmente mais um valor
-                            adicional, que diminuirá gradualmente ao longo de cinco anos, até a extinção do beneficio,
-                            isso só será aplicado quando a pessoa entrar em um emprego.
-                            Essas ações visam reduzir o custo de vida, impulsionar o crescimento econômico e promover a
-                            inclusão social.
+                            Para fortalecer a economia, nosso governo adotará as seguintes medidas: zeraremos o imposto
+                            sobre a cesta básica, tornando os alimentos essenciais mais acessíveis. Reduziremos o
+                            imposto geral para estimular a demanda, o que gerará mais empregos. Eliminaremos o IPI e
+                            outros impostos similares para aliviar o custo dos produtos. Além disso, reformularemos o
+                            Bolsa Família, garantindo que os beneficiários recebam o salário integralmente, mais um
+                            valor adicional, que diminuirá gradualmente ao longo de cinco anos, até a extinção do
+                            benefício. Essa medida só será aplicada quando a pessoa entrar em um emprego. Essas ações
+                            visam reduzir o custo de vida, impulsionar o crescimento econômico e promover a inclusão
+                            social.
                         </p>
                     </div>
                 </div>
@@ -127,12 +121,12 @@
                         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">EDUCAÇÃO
                         </h1>
                         <p class="mt-6 text-xl leading-8 text-gray-700">
-                            Aumento do salario dos professores, para consequentemente aumentar o nível de interesse
-                            social por cursos de licenciatura. professores qualificados (formados no curso em que dão
-                            aula) nas escolas públicas. investimento em atividades práticas nas escolas. programas
-                            bilíngues que realmente funcionam a partir do fundamental 1. incentivo a pesquisas, livros e
-                            artigos científicos. ensino informativo sobre o funcionamento de profissões, cursos e
-                            determinadas faculdades a partir do 1 ano do e.m. programas de intercâmbio.
+                            Aumento do salário dos professores para, consequentemente, aumentar o interesse social por
+                            cursos de licenciatura. Professores qualificados (formados na área em que lecionam) nas
+                            escolas públicas. Investimento em atividades práticas nas escolas. Programas bilíngues
+                            eficazes a partir do Fundamental 1. Incentivo à produção de pesquisas, livros e artigos
+                            científicos. Ensino informativo sobre o funcionamento de profissões, cursos e determinadas
+                            faculdades a partir do 1º ano do Ensino Médio. Programas de intercâmbio.
                         </p>
                     </div>
                 </div>
@@ -152,11 +146,11 @@
                         <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">SEGURANÇA
                         </h1>
                         <p class="mt-6 text-xl leading-8 text-gray-700">
-                            A segurança está relacionada à educação, inflação, desemprego, família, e qualidade de vida.
-                            Sugere melhorias na educação pública para formar cidadãos conscientes, controle da inflação
-                            e desemprego para reduzir crimes, fortalecimento do conselho tutelar para apoiar famílias
-                            desestruturadas, maior inclusão da população no planejamento de medidas de segurança e
-                            conscientização sobre a importância do voto.
+                            A segurança está relacionada à educação, à inflação, ao desemprego, à família e à qualidade
+                            de vida. Propomos melhorias na educação pública para formar cidadãos conscientes, controle
+                            da inflação e do desemprego para reduzir crimes, fortalecimento do conselho tutelar para
+                            apoiar famílias desestruturadas, maior inclusão da população no planejamento de medidas de
+                            segurança e conscientização sobre a importância do voto.
                         </p>
                     </div>
                 </div>
@@ -173,17 +167,17 @@
                 class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
                 <div class="lg:pr-4">
                     <div class="lg:max-w-lg">
-                        <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">COMBUSTIVÉL
+                        <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">COMBUSTÍVEL
                         </h1>
                         <p class="mt-6 text-xl leading-8 text-gray-700">
-                            A proposta visa uma abordagem abrangente para o setor de combustíveis no Brasil. Primeiro,
-                            sugere a redução ou até a eliminação dos impostos federais sobre combustíveis, como PIS e
-                            COFINS, que foram recentemente reintroduzidos pelo governo, buscando assim aliviar o custo
-                            final para os consumidores. Além disso, destaca a necessidade de investir na conclusão das
-                            refinarias nacionais que estão paralisadas ou com obras atrasadas, o que permitiria ao país
-                            refinar uma maior parte do petróleo bruto internamente, reduzindo a dependência de refino
-                            externo.
-                            A proposta também inclui a criação de políticas de incentivo para a produção e refino de
+                            A proposta visa uma abordagem abrangente para o setor de combustíveis no Brasil.
+                            Primeiramente, sugere a redução ou até a eliminação dos impostos federais sobre
+                            combustíveis, como PIS e COFINS, que foram recentemente reintroduzidos pelo governo,
+                            buscando assim aliviar o custo final para os consumidores. Além disso, destaca a necessidade
+                            de investir na conclusão das refinarias nacionais que estão paralisadas ou com obras
+                            atrasadas, o que permitiria ao país refinar uma maior parte do petróleo bruto internamente,
+                            reduzindo a dependência do refino externo.
+                            A proposta também inclui a criação de políticas de incentivo para a produção e o refino de
                             petróleo no Brasil, oferecendo subsídios e facilitando o acesso ao crédito para
                             investimentos no setor. Isso estimularia a produção interna e fortaleceria a
                             autossuficiência energética do país. Finalmente, a proposta defende o desenvolvimento de uma
